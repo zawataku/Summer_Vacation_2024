2b5c9d4caccad1784649a909a8cdd518b10c4889
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-08-08 11:51:14 +0900

    2024-08-08:Java入門

---
+++ Java/Increment/Increment.java
@@ -0,0 +1,13 @@
+package Java.Increment;
+
+/**
+ * Increment
+ */
+public class Increment {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
